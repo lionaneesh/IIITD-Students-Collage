bcd605821959c1ec885e0f812c44811c85c4d6d5
Aneesh Dogra <lionaneesh@gmail.com>
2013-12-20 20:02:05 +0530

    Get 75x75 images.

--- get_photo_links.py
+++ get_photo_links.py
@@ -1,4 +1,5 @@
 import json
+import time
 from urllib2 import urlopen
 
 
@@ -9,11 +10,13 @@ fp.close()
 user_photos = {} # id -> [User's Name, Photo URL]
 
 for user in data["data"]:
-    print user
-    page = urlopen("http://graph.facebook.com/" + user["id"] + "?fields=picture")
-    page_data = json.loads(page.read())
-    photo_url = page_data["picture"]["data"]["url"]
-    user_photos[user["id"]] = [user["name"], photo_url]
+    try:
+   	page = urlopen("http://graph.facebook.com/" + user["id"] + "?fields=picture.height(75)")
+	page_data = json.loads(page.read())
+    	photo_url = page_data["picture"]["data"]["url"]
+    	user_photos[user["id"]] = [user["name"], photo_url]
+    except:
+	print "Not included ", user
 
 fp = open("user_photos.json", "w")
 fp.write(json.dumps(user_photos))
